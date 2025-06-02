cd2eb8fa1d473559b95af368692a4a6463da90c2
PradeepGorle4 <pradeepgorle4@gmail.com>
2025-06-02 11:56:58 +0530

    terraform-eks-expense

--- 40-eks/main.tf
+++ 40-eks/main.tf
@@ -52,8 +52,7 @@ module "eks" {
       iam_role_additional_policies = {
           AmazonEBSCSIDriverPolicy = "arn:aws:iam::aws:policy/service-role/AmazonEBSCSIDriverPolicy"
           AmazonEFSCSIDriverPolicy = "arn:aws:iam::aws:policy/service-role/AmazonEFSCSIDriverPolicy"
-          AmazonEKSLoadBalancingPolicy = "arn:aws:iam::aws:policy/AmazonEKSLoadBalancingPolicy"
-          AmazonEKSWorkerNodePolicy = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
+          AmazonEKSLoadBalancingPolicy = "arn:aws:iam::aws:policy/ElasticLoadBalancingFullAccess"
       }
     }
   }
